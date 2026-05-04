54936badaa8124a5facde787383bea3ba99baf72
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-05-04 19:34:45 +0200

    Update: eslint.config.js, prettier.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -10,23 +10,11 @@
  * @see {@link https://github.com/sponsors/tomaschochola} GitHub Sponsors
  */
 
-import { Eslint, selectors } from '@tomaschochola/tooling-eslint';
-import globals from 'globals';
+import { ESLint } from '@tomaschochola/tooling-eslint';
 
 // eslint-disable-next-line no-restricted-exports
-export default new Eslint()
-  .add({
-    files: [...selectors.globalEcmaScript],
-  })
-  .globals({
-    ...globals.node,
-    ...globals.es2024,
-  })
-  .ignores()
-  .ignores(['node_modules', 'dist'])
-  .recommended()
-  .typescript()
-  .stylistic()
-  .sonarjs()
-  .typescriptDisabled({ files: [...selectors.rootEcmaScript] })
-  .build();
+export default new ESLint()
+  .presetNode({ presetDefaultsOptions: { configNodeModulesIgnores: false } })
+  .configIgnores(['node_modules', 'dist'])
+  .presetTypeScript()
+  .buildConfig();
